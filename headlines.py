9b7c8e219c364b7625d7e43592a172d415d4bc8c
rgbosque <rgbosque@gmail.com>
2017-06-23 09:27:05 +0800

    with jinja templates

--- headlines.py
+++ headlines.py
@@ -1,6 +1,6 @@
 import feedparser
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -23,16 +23,7 @@ def cnn():
 
 def get_news(publication):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-    <body>
-        <h1>Headlines</h1>
-        <b>{0}</b></br>
-        <i>{1}</i></br>
-        <p>{2}</p></br>
-    </body></html>""".format(first_article.get("title"),
-                             first_article.get("published"),
-                             first_article.get("summary"))
+    return render_template('home.html', articles=feed['entries'])
 
 
 if __name__ == "__main__":
